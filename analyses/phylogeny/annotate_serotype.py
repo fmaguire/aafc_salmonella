5817cd36e7fcdb334b7863c4eb7032cb330a81eb
Finlay Maguire <finlaymaguire@gmail.com>
2018-08-31 15:15:44 -0300

    Add all data apart from pangenome and kover analyses

--- analyses/phylogeny/annotate_serotype.py
+++ analyses/phylogeny/annotate_serotype.py
@@ -1,4 +1,5 @@
 import ete3
+import copy
 import pandas as pd
 import seaborn as sns
 
@@ -8,19 +9,22 @@ tree = ete3.Tree('pangenome_snps.phylip.contree')
 serovars = pd.read_csv('../serotyping/final_serotypes.tsv', sep='\t', names=['Index', 'Serotype'])
 serovars['Index'] = serovars['Index'].astype(str)
 serovars = serovars.set_index('Index')
+print(serovars)
 
 for leaf in tree.iter_leaves():
     serovar = serovars.loc[leaf.name, 'Serotype']
     leaf.add_features(serovar=serovar)
 
-current_palette = sns.color_palette("hls", 7)
 
+sns.set_palette('colorblind')
 # no support for hsl straight out as far as I can tell
 #serovar_colours = {k: v for k,v in zip(aafc_serovar.unique(), current_palette)}
 
 sero_lut = dict(zip(['Kentucky', 'Hadar', 'Heidelberg',
                      'I:4,[5],12:i:', 'Enteritidis',
-                     'Typhimurium', 'Thompson', 'None'], sns.color_palette().as_hex()))
+                     'Typhimurium', 'Thompson', 'None'], sns.color_palette('colorblind').as_hex()))
+
+print(sero_lut)
 
 ts = ete3.TreeStyle()
 ts.mode = 'c'
@@ -101,20 +105,45 @@ enter.set_style(enter_style)
 
 
 
-   ## create a new label with a color attribute
-    #N = ete3.AttrFace("serovar", bgcolor=sero_lut[l.serovar])
-    ## label margins
-    #N.margin_top = N.margin_bottom = N.margin_left = 4.0
-    ## labels aligned to the same level
-    #l.add_face(N, 1, position='aligned')
-#
 # reroot on thompson
 thompson_node = tree.search_nodes(name='3193')[0]
 thompson_node.img_style['bgcolor'] = sero_lut['Thompson']
 
+# highlight the subsampled taxa
+
+subset_nodes = []
+sub_tree = copy.copy(tree)
+for i in "1760 3125 3145 3193 1778 3126 3166 3333 1797 3132 3179 3339 1893 3142 3186".split():
+    node = sub_tree.search_nodes(name=i)[0]
+    subset_nodes.append(node)
+    node.name = serovars.loc[node.name, 'Serotype'].replace(',', '_').replace('[', '').replace(']', '').replace(':', '') + '_' + node.name
+
+
+
+
+sub_tree.prune(subset_nodes)
+with open('../pangenome/anvio_subsample/phylogeny_data.txt', 'w') as fh:
+    fh.write('item_name\tdata_type\tdata_value\n')
+    fh.write('Core_SNP_Phylogeny\tnewick\t{}\n'.format(sub_tree.write(format=1)))
+
+
+
+
+
+
+for node in subset_nodes:
+    node.name = node.name + " * "
+
+
+
+
+
+
+
 tree.set_outgroup(thompson_node)
 for l in tree.iter_leaves():
     l.name = l.name + " (" + l.serovar + ")"
+
 
 ts.show_leaf_name = True
 #ts.show_branch_support = True
